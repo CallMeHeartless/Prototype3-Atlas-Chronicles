38e6d71b038ac3ca863fdc774ce4d8e4db5dca5d
Kerry <k.of.blades@gmail.com>
2019-03-26 09:39:07 +1300

    Construction of player prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+
+    // Component references
+    private CharacterController m_CharacterController;
+
+    // Start is called before the first frame update
+    void Start(){
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    // Calculate movement
+
+}
