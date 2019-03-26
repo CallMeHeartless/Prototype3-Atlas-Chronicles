18e7723de964fa755f7d31812561901d5c2d74c3
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2019-03-27 12:51:27 +1300

    collectables add level saved

---
+++ Assets/DisplayStat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayStat : MonoBehaviour
+{
+    bool showStats = false;
+    public GameObject maps;
+    public GameObject note;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!showStats)
+            {
+                showStats = true;
+                maps.SetActive(true);
+                note.SetActive(true);
+                maps.GetComponent<Text>().text = GameStats.MapsBoard[GameStats.LevelLoctation].ToString();
+                note.GetComponent<Text>().text = GameStats.NoteBoard[GameStats.LevelLoctation].ToString();
+            }
+            else
+            {
+                showStats = false;
+                maps.SetActive(false);
+                note.SetActive(false);
+            }
+           
+        }
+        }
+}
