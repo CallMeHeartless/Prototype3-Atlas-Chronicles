8c7596f7e78dd7540597a9a0117ffe828cdcd64b
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2019-03-27 14:18:48 +1300

    wave point now work on the compass

---
+++ Assets/Scripts/wavePointOnCompass.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wavePointOnCompass : MonoBehaviour
+{
+    public GameObject marker;
+    public Vector3 turn;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, (marker.transform.position - transform.position), out hit))
+        {
+            Debug.DrawRay(transform.position, (marker.transform.position - transform.position), Color.green);
+            turn = Vector3.Cross(Vector3.up, gameObject.transform.position-marker.transform.position);
+
+
+
+            Vector3 screenPoint = cam.WorldToViewportPoint(marker.transform.position);
+            bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+            Debug.Log(onScreen);
+
+            if (screenPoint.x < 0)
+            {
+                //set postion to the right
+                gameObject.GetComponent<RectTransform>().position = new Vector3(430, 399, 0);
+            }
+            else
+            {
+                if (screenPoint.x > 1)
+                {
+                    gameObject.GetComponent<RectTransform>().position = new Vector3(252, 399, 0);
+                    //set postion to the left
+                }
+                else
+                {
+                    float number = 252+(screenPoint.x*178);
+                    gameObject.GetComponent<RectTransform>().position = new Vector3(number, 399, 0);
+                }
+            }
+            Debug.Log(screenPoint.x );
+        }
+    }
+}
