993005369ad5a7445402a02ad49802abd6f40196
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2019-03-29 12:53:07 +1300

    stone thorwing math all done now to added it to player

---
+++ Assets/stone.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stone : MonoBehaviour
+{
+    public LineRenderer line;
+    private bool ground = false;
+    private Vector3 goal;
+    public int nextPoint = 2;
+    public float speed;
+    public Vector3[] dummy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //dummy=new Vector3[gameObject.GetComponentInParent<stoneArc>().size];
+       // gameObject.GetComponentInParent<LineRenderer>().GetPositions(dummy);
+        //gameObject.GetComponent<LineRenderer>().SetPositions(dummy);
+       //line = gameObject.GetComponent<LineRenderer>();
+        goal = line.GetPosition(nextPoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ground)
+        {
+            if (Vector3.Distance(transform.position, goal) <= 0.2f)//at loctation
+            {
+                nextPoint++;
+                
+                if (nextPoint != gameObject.GetComponentInParent<stoneArc>().size)
+                {
+                    goal = line.GetPosition(nextPoint);
+
+                }
+                else
+                {
+                    ground = true;
+                }
+
+            }
+            transform.position = Vector3.MoveTowards(transform.position, goal, speed * Time.deltaTime);
+
+        }
+       
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 1))
+            {
+                ground = true;
+            }
+        
+    }
+}
--- Assets/stoneArc.cs
+++ Assets/stoneArc.cs
@@ -13,6 +13,7 @@ public class stoneArc : MonoBehaviour
     public float increaser;
     public int maxVel;
     public float rotation;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,43 +33,86 @@ public class stoneArc : MonoBehaviour
         {
             GameObject newStone = GameObject.Instantiate(stone,transform.position,transform.rotation);
             float anglechange = Mathf.Tan(Mathf.Deg2Rad * angle);
-            newStone.GetComponent<Rigidbody>().AddForce(new Vector3(-(velcoity * anglechange) + velcoity * increaser, velcoity * anglechange* increaser, 0), ForceMode.Acceleration);
+            newStone.transform.parent = gameObject.transform;
+            newStone.transform.position = lineRend.GetPosition(1);
+            newStone.GetComponent<stone>().speed = velcoity;
+
+            newStone.GetComponent<stone>().dummy = new Vector3[size];
+            lineRend.GetPositions(newStone.GetComponent<stone>().dummy);
+
+            newStone.GetComponent<LineRenderer>().positionCount = size;
+            newStone.GetComponent<LineRenderer>().SetPositions(newStone.GetComponent<stone>().dummy);
+
+            //newStone.GetComponent<Rigidbody>().AddForce(, ForceMode.Force);
+            //new Vector3(,,);
+            //float rad = Mathf.Deg2Rad * anglechange;
+            //newStone.GetComponent<Rigidbody>().velocity = (new Vector3(((-velcoity * rad) + velcoity), velcoity * rad, 0));
         }
     }
 
     void mathing()
     {
         Vector3[] newLoctation = new Vector3[size];
+        newLoctation[0] = player.transform.position;
         for (int i = 1; i < size; i++)
         {
             //newLoctation[i] = new Vector3(i * velcoity, ()+2, 0);
             //float a = velcoity * velcoity * Mathf.Sin(2 * (Mathf.Deg2Rad * angle)) / 9.81f;
-            newLoctation[i] = point((float)i/ (float)size, maxVel);
+            newLoctation[i] = point((float)i/ (float)size );
+            newLoctation[i] += player.transform.position;
         }
        
 
         lineRend.positionCount = newLoctation.Length;
         lineRend.SetPositions(newLoctation);
     }
-    Vector3 point(float i, float max)
+    Vector3 point(float i )
     {
         Vector3 currentPoint;
         float rad = Mathf.Deg2Rad * angle;
-        currentPoint.x = i * max;
+        currentPoint.x = i * maxVel;
         currentPoint.y = currentPoint.x * Mathf.Tan(rad) - ((9.81f * currentPoint.x * currentPoint.x) / (2 * velcoity * velcoity * Mathf.Cos(rad) * Mathf.Cos(rad)));
-
+        currentPoint.z = 0;
         //rotate
         if (rotation != 0)
         {
-            currentPoint.z = i * max * (rotation / 90);
-            currentPoint.x = i * max * (90 / rotation);
+            
+            float tempx, tempz, radrot;
+            radrot = Mathf.Deg2Rad * rotation;
+            tempx = currentPoint.x * Mathf.Cos(radrot) - currentPoint.z * Mathf.Sin(radrot);
+            tempz = currentPoint.x * Mathf.Sin(radrot) + currentPoint.z * Mathf.Cos(radrot);
+
+            currentPoint.z = tempz;
+            currentPoint.x = tempx;
         }
-        else
-        {
-            currentPoint.z = 0;
-        }
+       
         
 
         return currentPoint;
+    }
+
+    Vector3 dummy()
+    {
+        Vector3 currentPoint;
+        float rad = Mathf.Deg2Rad * angle;
+        currentPoint.x = size * maxVel;
+        currentPoint.y = currentPoint.x * Mathf.Tan(rad) - ((9.81f * currentPoint.x * currentPoint.x) / (2 * velcoity * velcoity * Mathf.Cos(rad) * Mathf.Cos(rad)));
+        currentPoint.z = 0;
+        //rotate
+        if (rotation != 0)
+        {
+
+            float tempx, tempz, radrot;
+            radrot = Mathf.Deg2Rad * rotation;
+            tempx = currentPoint.x * Mathf.Cos(radrot) - currentPoint.z * Mathf.Sin(radrot);
+            tempz = currentPoint.x * Mathf.Sin(radrot) + currentPoint.z * Mathf.Cos(radrot);
+
+            currentPoint.z = tempz;
+            currentPoint.x = tempx;
+        }
+        return currentPoint;
+        
+
+
     }
 }
