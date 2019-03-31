b3982a0f6422755f787ce4588e0f35ff5436be0c
Kerry <k.of.blades@gmail.com>
2019-04-01 11:33:22 +1300

    Player prefab updates

--- Assets/Scripts/JumpPadController.cs
+++ Assets/Scripts/JumpPadController.cs
@@ -6,18 +6,18 @@ public class JumpPadController : MonoBehaviour {
     [SerializeField]
     private float m_fJumpForce = 10.0f;
 
-    //private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other) {
 
-    //    if (other.CompareTag("Player")) {
-    //        other.GetComponent<PlayerController>().SetPlayerVerticalVelocity(m_fJumpForce);
-    //        print("jump");
-    //    }
-    //}
-
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("Player")) {
-            collision.gameObject.GetComponent<PlayerController>().SetPlayerVerticalVelocity(m_fJumpForce);
+        if (other.CompareTag("Player")) {
+            other.GetComponent<PlayerController>().SetPlayerVerticalVelocity(m_fJumpForce);
             print("jump");
         }
     }
+
+    //private void OnCollisionEnter(Collision collision) {
+    //    if (collision.gameObject.CompareTag("Player")) {
+    //        collision.gameObject.GetComponent<PlayerController>().SetPlayerVerticalVelocity(m_fJumpForce);
+    //        print("jump");
+    //    }
+    //}
 }
