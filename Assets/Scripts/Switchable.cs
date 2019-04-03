4e9e101aed0646a276057c8c3cc9a693bec5971e
Kerry <k.of.blades@gmail.com>
2019-04-04 11:55:59 +1300

    Enabled material change for switchable objects

--- Assets/Scripts/Switchable.cs
+++ Assets/Scripts/Switchable.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class Switchable : MonoBehaviour
 {
+    [SerializeField]
+    private bool m_bChangeMaterialOnTag = false;
+    [SerializeField]
+    private Material m_AlternateMaterial;
+    private Material m_Material;
+
+    private void Start() {
+        m_Material = GetComponent<MeshRenderer>().material;
+    }
+
+    public void Tag() {
+        if (m_bChangeMaterialOnTag) {
+            GetComponent<MeshRenderer>().material = m_AlternateMaterial;
+        }
+    }
+
+    public void DeTag() {
+        if (m_bChangeMaterialOnTag) {
+            GetComponent<MeshRenderer>().material = m_Material;
+        }
+    }
 
 }
