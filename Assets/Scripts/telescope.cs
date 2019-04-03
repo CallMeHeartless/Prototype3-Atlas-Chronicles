d7fdfe58282c90c19aefd327b07ce1ed62d8d3f9
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2019-04-04 10:45:04 +1300

    telescope changes and remove for prototye

--- Assets/Scripts/telescope.cs
+++ Assets/Scripts/telescope.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class telescope : MonoBehaviour
 {
     // Start is called before the first frame update
-    int POVNormal = 60;//standed POV
+    int POVNormal = 40;//standed POV
     public float newPOV =0;
     bool looking = true;
     public float delay= 2;
@@ -20,11 +21,12 @@ public class telescope : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (delay <= 0)
+        if (delay >= 0)
         {
-            if (Input.GetKeyDown(KeyCode.Q))
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                UIInfo();
+                
+               // UIInfo();
             }
             else
             {
@@ -33,44 +35,43 @@ public class telescope : MonoBehaviour
 
 
         }
-        void updateNewView()
-        {
-            if (looking)
-            {
-                StartCoroutine("zoom");  //gameObject.GetComponent<Camera>().fieldOfView = newPOV;
-            }
-            else
-            {
-                transform.GetChild(0).gameObject.SetActive(false);
-                camera.GetComponent<Camera>().fieldOfView = POVNormal;
-            }
-            looking = !looking;
-            loading = 2;
+        //void updateNewView()
+        //{
+        //    if (looking)
+        //    {
+        //        StartCoroutine("zoom");  //gameObject.GetComponent<Camera>().fieldOfView = newPOV;
+        //    }
+        //    else
+        //    {
+        //        transform.GetChild(0).gameObject.SetActive(false);
+        //        camera.GetComponent<CinemachineFreeLook>().m_Lens.FieldOfView = POVNormal;
+        //    }
+        //    looking = !looking;
+        //    loading = 2;
 
 
-        }
+        //}
 
         void UIInfo()
         {
-            if (delay <= 0)
+            if (delay >= 0)
             {
-                if (Input.GetKeyDown(KeyCode.Q))
-                {
+               
                     if (looking)
                     {
-                        camera.GetComponent<Camera>().fieldOfView = newPOV;
+                        camera.GetComponent<CinemachineFreeLook>().m_Lens.FieldOfView = newPOV;
                         transform.GetChild(0).gameObject.SetActive(true);
                         transform.GetChild(1).gameObject.SetActive(false);
                     }
                     else
                     {
-                        camera.GetComponent<Camera>().fieldOfView = POVNormal;
+                        camera.GetComponent<CinemachineFreeLook>().m_Lens.FieldOfView = POVNormal;
                         transform.GetChild(0).gameObject.SetActive(false);
                         transform.GetChild(1).gameObject.SetActive(true);
                     }
                     looking = !looking;
                     delay = 2;
-                }
+                
             }
             else
             {
@@ -86,6 +87,6 @@ public class telescope : MonoBehaviour
 
         yield return new WaitForSeconds(.5f);
         transform.GetChild(0).gameObject.SetActive(true);
-        camera.GetComponent<Camera>().fieldOfView = newPOV;
+        camera.GetComponent<CinemachineFreeLook>().m_Lens.FieldOfView = newPOV;
     }
 }
